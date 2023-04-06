1064daa8380f47bb2a5b4e5d38578e1638130baf
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-04-06 20:32:30 -0300

    Update sistema.php

--- interfaces/sistema.php
+++ interfaces/sistema.php
@@ -55,33 +55,24 @@
     </nav>
 
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+        <thead>
+            <tr>
+            <th scope="col">id</th>
+            <th scope="col">nomeFirst</th>
+            <th scope="col">telefone</th>
+            <th scope="col">email</th>
+            <th scope="col">sexo</th>
+            <th scope="col">data_nasc</th>
+            <th scope="col">cidade</th>
+            <th scope="col">estado</th>
+            <th scope="col">senha</th>
+            <th scope="col">...</th>
+
+            </tr>
+        </thead>
+        <tbody>
+           
+        </tbody>
+    </table>
     </body>
 </html>
