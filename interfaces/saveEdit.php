b2bb12f38924a758791c2ebd1bf8c31530ceabab
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-04-09 11:18:57 -0300

    Update saveEdit.php

--- interfaces/saveEdit.php
+++ interfaces/saveEdit.php
@@ -2,6 +2,13 @@
     include_once('../controller/config.php');
 
     if($_POST['update']){
-        
+        $nome = $_POST["nome"];
+        $email =$_POST["email"];
+        $telefone =$_POST["telefone"];
+        $sexo =$_POST["genero"];
+        $senha= $_POST["senha"];
+        $data_nasc = $_POST['data_nascimento'];
+        $cidade= $_POST["cidade"];
+        $estado =$_POST["estado"];
     }
 ?>
